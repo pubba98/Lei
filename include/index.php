45d60b71dccd54db41b137e5c0fb511ced0a38c0
pubba98 <coolpubba@gmail.com>
2020-11-10 09:19:41 +1300

    menu page included

--- include/index.php
+++ include/index.php
@@ -2,9 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Include Exercise</title>
+
 </head>
 <body>
+<div>id="header">
+<?php include "menu.php";?>,
+Login,pictures
+</div>
 
 </body>
 </html>
