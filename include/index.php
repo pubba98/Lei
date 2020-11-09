1d8864c4766f439022d63b50aa969b289ec35a6c
pubba98 <coolpubba@gmail.com>
2020-11-10 09:44:13 +1300

    menu page included

--- include/index.php
+++ include/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Include Exercise</title>
-    <link rel="stylesheet" href="css/style.css_">
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
